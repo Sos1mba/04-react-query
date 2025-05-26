495ac1f31cc02021534a10bebe193260eb0362f8
Vlad_Deineko <vladd1904@gmail.com>
2025-05-27 00:03:32 +0300

    update

---
+++ src/services/movieService.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+import type { Movie } from '../types/movie';
+
+
+interface MovieResponce {
+    results: movie[];
+}
+
+
+export const fetchMovies = async (topic: string): Promise<Movie[]> => {
+  const response = await axios.get<MoviesResponse>(
+    'https://api.themoviedb.org/3/search/movie',
+    {
+      params: { query },
+      headers: {
+        Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwOTExY2RkMDg1OTdjNDhkODVjMzAxNjcxZmZjOWRhNCIsIm5iZiI6MTc0ODI2OTg0MC43NTQsInN1YiI6IjY4MzQ3YjEwNDQwYzA3OGM2YmI2YjhiZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LkEAHPXcEy94otQvDCywlp1CIn4zQNjgSYtjmBjNfbQ`,
+      },
+    }
+  );
+  return response.data.results;
+};
